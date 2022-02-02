8ecd75141ab71d9e1dc78a633c3c2570c69976bf
Marcus Hyett (PostHog) <85295485+marcushyett-ph@users.noreply.github.com>
2022-02-02 14:50:43 +0000

    Upgraded Make Some Noise with Funnels
    Added more events representing an e-commerce funnel. This will create more realistic data that's easier to work with and more representative of a real product

--- index.ts
+++ index.ts
@@ -1,73 +1,122 @@
-import faker from 'faker'
+import faker from "faker";
 
-const rnd = (nr: number) => Math.ceil(Math.random() * nr) - 1
+const rnd = (nr) => Math.ceil(Math.random() * nr) - 1;
 
-const urls = ['/project/plugins', '/events', '/insights', '/signup', '/']
+const eventTypes = [
+  {
+    name: "user_signup",
+    url: "/",
+  },
+  {
+    name: "browse_catalog",
+    url: "/",
+  },
+  {
+    name: "product_impresssion",
+    url: "/",
+  },
+  {
+    name: "product_description_view",
+    url: "/product",
+  },
+  {
+    name: "add_to_cart",
+    url: "/cart",
+  },
+  {
+    name: "checkout",
+    url: "/checkout",
+  },
+];
+
+const initial_referrers = [
+  "google.com",
+  "bing.com",
+  "news.com",
+  "facebook.com",
+];
+const productIDs = [
+  12213, 231240, 1249210, 213221, 43242, 93204, 123123, 3423, 124124, 3902034,
+  312131, 42039402, 123123, 12239,
+];
 
 export async function runEveryMinute({ config }) {
-    const count = parseInt(config.amplification) || 1
-    for (let i = 0; i < count; i++) {
-        await makeNoise()
-    }
+  const count = parseInt(config.amplification) || 1;
+  for (let i = 0; i < count; i++) {
+    await makeNoise();
+  }
 }
 
 export async function setupPlugin() {
-    // make some noise on start
-    await makeNoise()
+  // make some noise on start
+  await makeNoise();
 }
 
-async function makeNoise() {
-    const randomChoice = rnd(2)
-    const userData = faker.helpers.contextualCard()
-    
-    await posthog.capture('user signup', { ...userData, distinct_id: userData.username })
-    if (randomChoice === 1) {
-        const path = urls[rnd(urls.length)]
-        await posthog.capture('$pageview', {
-            $active_feature_flags: [],
-            $browser: 'Chrome',
-            $browser_version: 92,
-            $current_url: `http://localhost:8000${path}`,
-            $device_id: 'random-device-id-in-an-uuidish-format',
-            $device_type: 'Desktop',
-            $geoip_city_name: faker.address.city(),
-            $geoip_continent_code: 'OC',
-            $geoip_continent_name: 'Oceania',
-            $geoip_country_code: 'AU',
-            $geoip_country_name: faker.address.country(),
-            $geoip_latitude: -33.8591,
-            $geoip_longitude: 151.2002,
-            $geoip_postal_code: '2000',
-            $geoip_subdivision_1_code: 'NSW',
-            $geoip_subdivision_1_name: 'New South Wales',
-            $geoip_time_zone: 'Australia/Sydney',
-            $host: 'localhost:8000',
-            $initial_referrer: `http://localhost:8000/${urls[rnd(urls.length)]}`,
-            $initial_referring_domain: 'localhost:8000',
-            $insert_id: '35waxaj8kcutva6l',
-            $ip: '127.0.0.1',
-            $lib: 'web',
-            $lib_version: '1.13.9',
-            $os: 'Mac OS X',
-            $pathname: path,
-            $plugins_deferred: [],
-            $plugins_failed: [],
-            $plugins_succeeded: ['GeoIP (1)'],
-            $referrer: `http://localhost:8000/${urls[rnd(urls.length)]}`,
-            $referring_domain: 'localhost:8000',
-            $screen_height: 900,
-            $screen_width: 1440,
-            $time: 1631350054.975,
-            $user_id: userData.username,
-            $viewport_height: 796,
-            $viewport_width: 789,
-            distinct_id: userData.username,
-            ee_available: true,
-            email_service_available: false,
-            is_clickhouse_enabled: true,
-            is_demo_project: false,
-            posthog_version: '1.27.0',
-            realm: 'hosted-clickhouse',
-        })
+makeNoise = async () => {
+  //Always include signup
+  const steps = rnd(eventTypes.length) + 1;
+  const product_id = productIDs[rnd(productIDs.length)];
+  const initial_referrer = initial_referrers[rnd(initial_referrers.length)];
+  const userData = faker.helpers.contextualCard();
+  //Run through event types in order
+  for (var i = 0; i < steps; i++) {
+    //simulate realistic time between events
+    const time_offset = rnd(100000);
+    const event_timestamp = +new Date() + time_offset * i;
+    //Always generate a pageview event
+    await submitEvent({
+      name: "$pageview",
+      timestamp: event_timestamp,
+      initial_referrer: initial_referrer,
+      referrer: i == 0 ? "/" : eventTypes[i - 1].url,
+      current_url: eventTypes[i].url,
+      userData: userData,
+    });
+
+    //For user_signup and browse catalog don't use a productID
+    if (!["user_signup", "browse_catalog"].includes(eventTypes[i].name)) {
+      await submitEvent({
+        name: eventTypes[i].name,
+        productID: product_id,
+        timestamp: event_timestamp,
+        initial_referrer: initial_referrer,
+        referrer: eventTypes[i - 1].url,
+        current_url: eventTypes[i].url,
+        userData: userData,
+      });
+    } else {
+      await submitEvent({
+        name: eventTypes[i].name,
+        timestamp: event_timestamp,
+        initial_referrer: initial_referrer,
+        referrer: "/",
+        current_url: eventTypes[i].url,
+        userData: userData,
+      });
     }
-}
+  }
+};
+
+submitEvent = async (event) => {
+  const resp = await posthog.capture(event.name, {
+    $timestamp: new Date(event.timestamp),
+    $browser: "Chrome",
+    $browser_version: 100,
+    $current_url: `http://amazingstore.com${event.current_url}`,
+    $device_type: "Desktop",
+    $geoip_city_name: event.userData.address.city,
+    $host: "http://localhost:8000",
+    $initial_referrer: `http://${event.initial_referrer}`,
+    $initial_referring_domain: `http://${event.initial_referrer}`,
+    $os: "Mac OS X",
+    $pathname: event.current_url,
+    $referrer: `http://amazingstore.com${event.referrer}`,
+    $screen_height: 900,
+    $screen_width: 1440,
+    $time: (event.timestamp / 1000),
+    $user_id: event.userData.username,
+    distinct_id: event.userData.username,
+    product_id: event.productID? event.productID:  null, 
+    $set: { email: event.userData.email}
+  });
+};
